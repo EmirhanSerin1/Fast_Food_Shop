442d1abf6b1ae19e0e2e388b32d02b1993f180ff
Emirhan <emirhanserin.1@gmail.com>
2021-07-27 15:38:22 +0300

    init

---
+++ android/app/src/main/kotlin/com/example/fast_food_shop/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fast_food_shop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/home/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({ Key? key }) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
