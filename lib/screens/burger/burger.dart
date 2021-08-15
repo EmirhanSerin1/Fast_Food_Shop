011c9a13ead367dcf6dc6cc88e95ea4452e71a5d
Emirhan <emirhanserin.1@gmail.com>
2021-08-15 21:14:37 +0300

    push

--- lib/screens/burger/burger.dart
+++ lib/screens/burger/burger.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class Burger extends StatefulWidget {
@@ -10,6 +12,9 @@ class Burger extends StatefulWidget {
 }
 
 class _BurgerState extends State<Burger> {
+  var netPrice = 0;
+  var quantity = 1;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -50,10 +55,143 @@ class _BurgerState extends State<Burger> {
                         ),
                       ),
                     ),
+                    Positioned(
+                      top: 1,
+                      right: 4,
+                      child: Container(
+                        height: 12,
+                        width: 12,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          shape: BoxShape.circle,
+                        ),
+                        child: Center(
+                          child: Text(
+                            "1",
+                            style: TextStyle(fontSize: 9, color: Color(0xFFF56953)),
+                          ),
+                        ),
+                      ),
+                    )
                   ],
                 )
               ],
             ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(16, 16, 0, 0),
+            child: Text(
+              "CRAZY",
+              style: TextStyle(
+                fontSize: 26,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 16),
+            child: Text(
+              widget.foodName.toString().toUpperCase(),
+              style: TextStyle(
+                fontSize: 26,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ),
+          SizedBox(height: 40),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Hero(
+                tag: widget.heroTag,
+                child: Container(
+                  height: 200,
+                  width: 200,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                    image: AssetImage(widget.imagePath),
+                    fit: BoxFit.contain,
+                  )),
+                ),
+              ),
+              SizedBox(width: 15),
+              Column(
+                children: [
+                  Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(16),
+                        boxShadow: [
+                          BoxShadow(
+                            offset: Offset(1, 3),
+                            blurRadius: 4,
+                            color: Color(0xFFF56953).withOpacity(0.2),
+                          ),
+                        ],
+                        color: Color(0xFFFFFFFF)),
+                    child: Icon(
+                      Icons.favorite_border_outlined,
+                      color: Color(0xFFF56953),
+                    ),
+                  ),
+                  SizedBox(height: 40),
+                  Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(16),
+                        boxShadow: [
+                          BoxShadow(
+                            offset: Offset(1, 3),
+                            blurRadius: 4,
+                            color: Color(0xFFF56953).withOpacity(0.2),
+                          ),
+                        ],
+                        color: Color(0xFFFFFFFF)),
+                    child: Icon(
+                      Icons.restore,
+                      color: Color(0xFFF56953),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+          SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                height: 70,
+                width: 120,
+                color: Colors.white,
+                child: Center(
+                  child: Text(
+                    "\$" + (int.parse(widget.price) + quantity).toString(),
+                    style: TextStyle(
+                      fontSize: 40,
+                      color: Color(0xFF484A4E),
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                height: 60,
+                width: 225,
+                decoration: BoxDecoration(
+                    color: Color(0xFFF56953),
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      bottomLeft: Radius.circular(10),
+                    )),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [],
+                ),
+              )
+            ],
           )
         ],
       ),
