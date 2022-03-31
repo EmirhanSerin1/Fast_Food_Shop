88bd1fd27e41a557801d6e7238947bfd234fd0cd
EmirhanSerin1 <emirhanserin.1@gmail.com>
2022-03-31 23:42:53 +0200

    update who am I

--- lib/screens/who_am_I/who_am_I.dart
+++ lib/screens/who_am_I/who_am_I.dart
@@ -12,8 +12,23 @@ class _WhoAmIState extends State<WhoAmI> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Center(
-        child: Text("I am still developing this page"),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Text("I am still developing"),
+//           Text("""I am ambitious and hardworking student who wants to
+// gain experience while studying at University. I'm
+// learning Flutter for the past 2 years. I'm very passionate
+// about app developing. I create my own flutter projects.
+// You can see all of them in my github account. I like to
+// learn new things so in my spare time I'm taking extra
+// courses about Java Programming. I'm confident in
+// frontend and interface design. I can use Mvvm, State
+// Management, Firebase, GetX, MobX. I look forward for
+// new challenges.
+// In the future I see myself as a successful mobile
+// developer"""),
+        ),
       ),
       
     );
